83dad0ae76d1efb0634c9d42f56a44907877b4eb
고경남 <rhrudska987@naver.com>
2023-11-05 17:17:26 +0900

    feat: 프로젝트 세팅
    build 파일 및 yml 파일 추가

---
+++ src/main/java/com/example/coin_trade/CoinTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coin_trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinTradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coin_trade/CoinTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coin_trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
