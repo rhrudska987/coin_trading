2b78f93e48fe274f385769d6825d1eeeaf9a1fb0
고경남 <rhrudska987@naver.com>
2023-11-28 09:50:13 +0900

    refactor: Repository 수정
    코인 종류에 따른 정보 가져오는 함수 추가

--- src/main/java/com/example/coin_trade/domain/coin/CoinRepository.java
+++ src/main/java/com/example/coin_trade/domain/coin/CoinRepository.java
@@ -1,6 +1,14 @@
 package com.example.coin_trade.domain.coin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CoinRepository extends JpaRepository<Coin, Long> {
+
+//    Coin findBycoin_code(String coin_code);
+
+    @Query("select c from Coin c where c.coin_code = :coin_code")
+    Coin findByCoin_code(@Param("coin_code") String coin_code);
+
 }
