05e3f11e22a48834474c2b0d43063c05a5d1df1b
고경남 <rhrudska987@naver.com>
2023-11-28 09:50:55 +0900

    refactor: 메인 파일 수정
    @EnableScheduling 기능 추가

--- src/main/java/com/example/coin_trade/CoinTradeApplication.java
+++ src/main/java/com/example/coin_trade/CoinTradeApplication.java
@@ -2,8 +2,10 @@ package com.example.coin_trade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoinTradeApplication {
 
     public static void main(String[] args) {
