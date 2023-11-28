68286e5d5463935ecc093832825cfc11bd81cbce
고경남 <rhrudska987@naver.com>
2023-11-28 09:49:58 +0900

    refactor: Repository 수정
    코인 종류에 따른 정보 가져오는 함수 추가

--- src/main/java/com/example/coin_trade/domain/price/PriceRepository.java
+++ src/main/java/com/example/coin_trade/domain/price/PriceRepository.java
@@ -1,6 +1,13 @@
 package com.example.coin_trade.domain.price;
 
+import com.example.coin_trade.domain.coin.Coin;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PriceRepository extends JpaRepository<Price, Long> {
+
+    List<Price> findByCoinOrderByCoinDesc(Coin coin);
+
+    Long countByCoin(Coin coin);
 }
--- src/main/java/com/example/coin_trade/domain/wallet/WalletRepository.java
+++ src/main/java/com/example/coin_trade/domain/wallet/WalletRepository.java
@@ -1,6 +1,15 @@
 package com.example.coin_trade.domain.wallet;
 
+import com.example.coin_trade.domain.coin.Coin;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    @Query("select w from Wallet w where w.coin = :coin order by w.id desc")
+    List<Wallet> findAllByCoin(@Param("coin") Coin coin);
+
 }
